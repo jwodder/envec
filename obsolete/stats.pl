a1d4f3a80c6e11d643eca130dcac7fb3f2056f88
John T. Wodder II <jwodder@sdf.lonestar.org>
2012-01-01 17:12:52 -0500

    Deleted obsolete/

--- obsolete/stats.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl -w
-use strict;
-use EnVec 'parseJSON';
-
-$/ = undef;
-print join("\t", map { defined $_ ? $_ : '' } $_->name, $_->type, $_->cost,
-					      $_->indicator, $_->PT,
-					      $_->loyalty, $_->HandLife), "\n"
- for map { $_->isSplit ? ($_->part1, $_->part2) : $_ } @{parseJSON <>};
