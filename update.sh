a4633308072accbf8d116718f668124a2410fcda
John T. Wodder II <jwodder@sdf.lonestar.org>
2012-04-29 18:32:06 -0400

    Worked on update.sh and marked Unglued & Unhinged as importable

--- update.sh
+++ update.sh
@@ -1,27 +1,23 @@
 #!/bin/sh
 setfile=data/sets.tsv
-details=out/details
 
-currSet=`awk -F'\t+' '/^[^#]/ { print $3 "\t" $1 }' $setfile | sort -r | head -n1 | cut -f2`
-dir=$details/`date +%Y%m%d`-$currSet
-mkdir -p "$dir"
+currSet=`awk -F'\t+' '/^[^#]/ { print $3 "\t" tolower($1) }' $setfile | sort -r | head -n1 | cut -f2`
+dir=`date +%Y%m%d`-$currSet
+echo Creating "$dir"
+mkdir -p "out/$dir"
 
 [ -e ids.txt ] || perl uniqIDs.pl $setfile > ids.txt || exit
-
 date +'Start: %s'
-perl details.pl -C ids.txt -l details.log -S $setfile \
-		-j "$dir/details.json" -x "$dir/details.xml" || exit
+perl details.pl -S $setfile -C ids.txt -l details.log \
+		-j "out/$dir/details.json" -x "out/$dir/details.xml" || exit
 date +'End: %s'
+perl toText1.pl "out/$dir/details.json" > out/cards2.txt
 
-#rm ids.txt
+cd out
+diff -Nu details0.json "$dir/details.json" > details.json.diff
+diff -Nu details0.xml  "$dir/details.xml"  > details.xml.diff
+rm -f details0.json details0.xml
+ln -s "$dir/details.json" details0.json
+ln -s "$dir/details.xml"  details0.xml
 
-perl toText1.pl "$dir/details.json" > out/cards2.txt
-
-[ -e out/details0.json ] && diff -u out/details0.json "$dir/details.json" > out/details.json.diff
-[ -e out/details0.xml ] && diff -u out/details0.xml "$dir/details.xml" > out/details.xml.diff
-
-#rm -f out/details0.json out/details0.xml
-#ln -s "$dir/details.json" ...
-#ln -s "$dir/details.xml" ...
-
-### Regenerate lists.txt too?
+### Regenerate lists.txt
