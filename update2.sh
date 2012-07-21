0d753ae044d7c83d1078b362468f410b8432178b
John T. Wodder II <jwodder@sdf.lonestar.org>
2012-07-21 04:15:20 +0000

    Fixed a bug in update2.sh

--- update2.sh
+++ update2.sh
@@ -3,10 +3,11 @@
 setfile=data/sets.tsv
 currSet=`awk -F'\t+' '/^[^#]/ { print $3 "\t" tolower($1) }' $setfile | sort -r | head -n1 | cut -f2`
 dir=`date +%Y%m%d`-$currSet
-echo Creating "out/$dir"
+#echo Creating "out/$dir"
 mkdir -p "out/$dir"
 
-if [ -e ids.txt ] then Ci=-C else Ci=-i fi
+Ci=-C
+[ -e ids.txt ] || Ci=-i
 perl details.pl -S $setfile $Ci ids.txt -l details.log \
 		-j "out/$dir/details.json" -x "out/$dir/details.xml" || exit
 perl toText1.pl "out/$dir/details.json" > out/cards2.txt
