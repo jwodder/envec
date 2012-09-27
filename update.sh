4f33ae26a942d383d48253b9219fc53d58090039
John T. Wodder II <jwodder@sdf.lonestar.org>
2012-09-27 15:20:57 -0400

    update.sh now outputs the details files to out/YYYYMMDD-set.{json,xml} rather than out/YYYYMMDD-set/details.{json,xml}

--- update.sh
+++ update.sh
@@ -2,21 +2,21 @@
 # invoke with: nice ./update.sh &
 setfile=data/sets.tsv
 currSet=`awk -F'\t+' '/^[^#]/ { print $3 "\t" tolower($1) }' $setfile | sort -r | head -n1 | cut -f2`
-dir=`date +%Y%m%d`-$currSet
-mkdir -p "out/$dir"
+base=`date +%Y%m%d`-$currSet
 
 Ci=-C
 [ -e ids.txt ] || Ci=-i
+mkdir -p out
 perl details.pl -S $setfile $Ci ids.txt -l details.log \
-		-j "out/$dir/details.json" -x "out/$dir/details.xml" || exit
-perl toText1.pl "out/$dir/details.json" > out/cards2.txt
-chmod -w out/cards2.txt
-perl listify.pl -o out/cardlists2.txt "out/$dir/details.json"
+		-j "out/$base.json" -x "out/$base.xml" || exit
+perl toText1.pl "out/$base.json" > out/cards2.txt
+perl listify.pl -o out/cardlists2.txt "out/$base.json"
 echo '# vim:set nowrap:' >> out/cardlists2.txt
+chmod -w "out/$base.json" "out/$base.xml" out/cards2.txt out/cardlists2.txt
 
 #cd out
-#diff -Nu details0.json "$dir/details.json" > details.json.diff
-#diff -Nu details0.xml  "$dir/details.xml"  > details.xml.diff
-#rm -f details0.json details0.xml
-#ln -s "$dir/details.json" details0.json
-#ln -s "$dir/details.xml"  details0.xml
+#diff -Nu details.json "$base.json" > details.json.diff
+#diff -Nu details.xml  "$base.xml"  > details.xml.diff
+#rm -f details.json details.xml
+#ln -s "$base.json" details.json
+#ln -s "$base.xml"  details.xml
