fb6b884ecf4b647fc72b54100aaf553343fccba8
John T. Wodder II <jwodder@sdf.lonestar.org>
2014-07-03 20:26:46 -0400

    Removed the functionality from JSON.pm for serializing & deserializing card databases as objects rather than lists

--- EnVec/JSON.pm
+++ EnVec/JSON.pm
@@ -6,7 +6,7 @@ use JSON::Syck;
 use EnVec::Card;
 use EnVec::Util 'jsonify', 'openR';
 use Exporter 'import';
-our @EXPORT_OK = qw< dumpArray dumpHash parseJSON loadJSON >;
+our @EXPORT_OK = qw< dumpArray parseJSON loadJSON >;
 our %EXPORT_TAGS = (all => \@EXPORT_OK);
 
 sub dumpArray(\@;$) {  # Should it be ($;$) instead?
@@ -22,28 +22,11 @@ sub dumpArray(\@;$) {  # Should it be ($;$) instead?
  print $fh "\n]\n";
 }
 
-sub dumpHash(\%;$$) {  # Should it be ($;$$) instead?
- my($hash, $sort, $fh) = @_;
- $fh ||= select;
- print $fh "{\n";
- my $first = 1;
- for ($sort ? sort keys %$hash : keys %$hash) {
-  print $fh ",\n\n" if !$first;
-  print $fh ' ', jsonify($_), ': ', $hash->{$_}->toJSON;
-  $first = 0;
- }
- print $fh "\n}\n";
-}
-
 sub parseJSON($) {  # Load from a string
  my $data = JSON::Syck::Load(shift);
  croak "EnVec::JSON::parseJSON: could not parse input\n" if !defined $data;
  if (ref $data eq 'ARRAY') { [ map { EnVec::Card->fromHashref($_) } @$data ] }
- elsif (ref $data eq 'HASH') {
-  +{ map { $_ => EnVec::Card->fromHashref($data->{$_}) } keys %$data }
- } else {
-  croak "EnVec::JSON::parseJSON: root structure must be an array or object"
- }
+ else { croak "EnVec::JSON::parseJSON: root structure must be an array" }
 }
 
 sub loadJSON(;$) {  # Load from a file (identified by name) or stdin
