14aaef9eb6b38ec55bc3d2950cd97c989b810d1e
John T. Wodder II <jwodder@sdf.lonestar.org>
2014-09-04 17:58:16 -0400

    Gave fetch.sh an -o option for setting the output directory, a -b option for setting the basename for the JSON & XML output files, and command-line argument for setting the setfile

--- fetch.sh
+++ fetch.sh
@@ -1,14 +1,32 @@
-#!/bin/sh
+#!/bin/bash
 # Recommended invocation: nice ./fetch.sh &
 dir=fetched
-setfile=data/sets.tsv
-currSet=`awk -F'\t+' '/^[^#]/ { print $3 "\t" tolower($1) }' $setfile | sort -r | head -n1 | cut -f2`
-base=`date -u +%Y%m%d`-$currSet
+args=`getopt o:b: $*` || {
+ echo "Usage: $0 [-o dir] [-b base] [setfile]";
+ exit 2;
+}
+set -- $args
+for i
+do case "$1" in
+ -o) dir="$2"; shift; shift;;
+ -b) base="$2"; shift; shift;;
+ --) shift; break;;
+esac done
+
+if [ -n "$1" ]
+then setfile="$1"
+else setfile=data/sets.tsv
+fi
+
+if [ -z "$base" ]
+then currSet=`awk -F'\t+' '/^[^#]/ { print $3 "\t" tolower($1) }' $setfile | sort -r | head -n1 | cut -f2`
+     base=`date -u +%Y%m%d`-$currSet
+fi
 
 Ci=i
 [ -e "$dir/ids.txt" ] && Ci=C
 
-mkdir -p $dir
+mkdir -p "$dir"
 perl tutor.pl -S "$setfile" \
 	      -$Ci "$dir/ids.txt" \
 	      -l "$dir/tutor.log" \
