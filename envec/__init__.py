963d1e9cca8e65b0fbc1548e0347cb7fdc87c98f
John T. Wodder II <jwodder@sdf.lonestar.org>
2015-10-18 17:48:45 -0400

    Rewrote envec/sets.py to support the new set file and then some

--- envec/__init__.py
+++ envec/__init__.py
@@ -4,4 +4,4 @@ from .colors    import Color
 from .details   import parseDetails, loadDetails
 from .json      import dumpArray, parseJSON, loadJSON
 from .multipart import MultipartDB, CardClass, loadParts, getMultipartDB
-from .sets      import CardSet, CardSetDB, loadSets, getCardSetDB
+from .sets      import CardSet, CardSetDB
