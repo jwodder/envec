1dc5bc28268dda3aa326f54b5afd042cd6c4307c
John T. Wodder II <jwodder@sdf.lonestar.org>
2015-10-19 22:42:05 -0400

    Added a Rarity class

--- envec/__init__.py
+++ envec/__init__.py
@@ -1,7 +1,8 @@
 from .card      import Card
+from .cardset   import CardSet, CardSetDB
 from .checklist import parseChecklist, loadChecklist
 from .color     import Color
 from .details   import parseDetails, loadDetails
 from .json      import dumpArray, parseJSON, loadJSON
 from .multipart import MultipartDB, CardClass
-from .cardset   import CardSet, CardSetDB
+from .rarity    import Rarity
---
+++ envec/rarity.py
@@ -0,0 +1,42 @@
+from enum import Enum
+
+class Rarity(Enum):
+    land        = ('L', 'Land')
+    L           = ('L', 'Land')
+
+    common      = ('C', 'Common')
+    C           = ('C', 'Common')
+
+    uncommon    = ('U', 'Uncommon')
+    U           = ('U', 'Uncommon')
+
+    rare        = ('R', 'Rare')
+    R           = ('R', 'Rare')
+
+    mythic_rare = ('M', 'Mythic Rare')
+    M           = ('M', 'Mythic Rare')
+    MR          = ('M', 'Mythic Rare')
+    mythic      = ('M', 'Mythic Rare')
+    mythicrare  = ('M', 'Mythic Rare')
+
+    special     = ('S', 'Special')
+    S           = ('S', 'Special')
+
+    bonus       = ('B', 'Bonus')
+    B           = ('B', 'Bonus')
+
+    promo       = ('P', 'Promo')
+    P           = ('P', 'Promo')
+    promotional = ('P', 'Promo')
+
+    def __init__(self, shortname, longname):
+        self.shortname = shortname
+        self.longname  = longname
+
+    @classmethod
+    def fromString(cls, name):
+        name = name.replace('-', '_')
+        if len(name) <= 2:
+            return cls[name.upper()]
+        else:
+            return cls[name.lower()]
