33e692d36631e9f6d89fd49294b5b33cdf69b3f3
John T. Wodder II <jwodder@sdf.lonestar.org>
2015-11-13 15:50:46 +0000

    Compressed `parseChecklist` a bit

--- envec/checklist.py
+++ envec/checklist.py
@@ -4,26 +4,24 @@ from   bs4    import BeautifulSoup
 
 def parseChecklist(obj):
     ### TODO: Make this use `yield`?
-    ### TODO: Return namedtuples instead of dicts
     cards = []
     doc = BeautifulSoup(obj, 'html.parser')
-    for table in doc.find_all('table'):
-        if 'checklist' not in table['class']:
-            continue
-        for tr in table.find_all('tr'):
-            if 'cardItem' not in tr['class']:
-                continue
-            item = {}
-            for td in tr.find_all('td'):
-                key = td['class'][0]
-                #value = td.get_text()
-                value = simplify(magicContent(td))
-                item[key] = value
-                if key == 'name':
+    for tr in doc.find('table', class_='checklist')\
+                 .find_all('tr', class_='cardItem'):
+        item = {}
+        for td in tr.find_all('td'):
+            key = ' '.join(td['class'])
+            #value = td.get_text()
+            value = simplify(magicContent(td))
+            item[key] = value
+            if key == 'name':
+                try:
                     url = td.a['href']
-                    if url:
-                        m = re.search(r'\bmultiverseid=(\d+)', url)
-                        if m:
-                            item['multiverseid'] = m.group(1)
-            cards.append(item)
-        break
+                except (TypeError, KeyError):
+                    pass
+                else:
+                    m = re.search(r'\bmultiverseid=(\d+)\b', url)
+                    if m:
+                        item['multiverseid'] = m.group(1)
+        cards.append(item)
+    return cards
