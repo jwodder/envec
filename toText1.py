afe628c44f3502c17b7fa8a93929734c629990db
John T. Wodder II <jwodder@sdf.lonestar.org>
2015-11-15 16:53:14 +0000

    Handle Unicode output in toText1.py

--- toText1.py
+++ toText1.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python
 import argparse
+import codecs
 import sys
 from   envec import iloadJSON
 
 parser = argparse.ArgumentParser()
 parser.add_argument('infile', type=argparse.FileType('r'), default=sys.stdin)
 args = parser.parse_args()
+
+sys.stdout = codecs.getwriter('utf-8')(sys.stdout)
 
 for card in iloadJSON(args.infile):
     print card.toText1(0,1)
