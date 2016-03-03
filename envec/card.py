6c447825ba7c4f4121837f22b9abd7d05ec4b762
John T. Wodder II <jwodder@sdf.lonestar.org>
2016-03-03 21:48:12 +0000

    Merged Content into Card

--- envec/card.py
+++ envec/card.py
@@ -1,10 +1,12 @@
 import copy
+from   functools  import total_ordering
 from   itertools  import groupby, starmap, zip_longest
+import re
 from   .content   import Content
 from   .printing  import Printing
 from   .color     import Color
 from   .multipart import CardClass
-from   ._util     import wrapLines, cheap_repr, for_json
+from   ._util     import wrapLines, cheap_repr, for_json, split_mana
 
 sep = ' // '
 
@@ -39,12 +41,28 @@ def scalarField(field):
             return sep.join(str(f) or '' for f in fields)
     return property(getter)
 
+@total_ordering
 class Card:
-    def __init__(self, cardClass, content, printings=None, rulings=None):
-        self.cardClass = cardClass
-        self.content   = list(content)         # list of envec.content objects
-        self.printings = list(printings or []) # list of envec.printing objects
-        self.rulings   = list(rulings or [])
+    def __init__(self, cardClass, name, types, cost=None, supertypes=(),
+                 subtypes=(), text=None, power=None, toughness=None,
+                 loyalty=None, hand=None, life=None, color_indicator=None,
+                 secondary=None, printings=None, rulings=None):
+        self.cardClass  = cardClass
+        self.name       = name                  # string
+        self.types      = tuple(types)          # tuple of strings
+        self.cost       = cost                  # string or None
+        self.supertypes = tuple(supertypes)     # tuple of strings
+        self.subtypes   = tuple(subtypes)       # tuple of strings
+        self.text       = text                  # string or None
+        self.power      = power                 # string or None
+        self.toughness  = toughness             # string or None
+        self.loyalty    = loyalty               # string or None
+        self.hand       = hand                  # string or None
+        self.life       = life                  # string or None
+        self.color_indicator = color_indicator  # Color or None
+        self.secondary  = secondary
+        self.printings  = list(printings or []) # list of envec.Printing objects
+        self.rulings    = list(rulings or [])
          # list of dicts with the following fields:
          #  - date
          #  - ruling
@@ -52,64 +70,97 @@ class Card:
          # TODO: Create a `namedtuple` Rulings class?
 
     @classmethod
-    def newCard(cls, **attrs):
-        content = {}
-        for field in ("name", "cost", "text", "power", "toughness", "loyalty",
-                      "hand", "life", "color_indicator", "supertypes", "types",
-                      "subtypes"):
-            if field in attrs:
-                content[field] = attrs[field]
-        attrs2 = attrs.copy()
-        attrs2["content"] = [content]
-        return cls.fromDict(attrs2)
-
-    @classmethod
     def fromDict(cls, obj):
         if isinstance(obj, cls):
             return copy.deepcopy(obj)
         ### TODO: Move all of these transformations to __init__?
         cardClass = CardClass[obj.get("cardClass", "normal")]
-        content = obj["content"]
-        if isinstance(content, (list, tuple)):
-            if not content:
-                raise ValueError("'content' field must be a nonempty list")
-            content = map(Content.fromDict, content)
-        else:
-            content = [Content.fromDict(content)]
-        printings = map(Printing.fromDict, obj.get("printings", ()))
-        rulings = obj.get("rulings", [])
-        return cls(cardClass, content, printings, rulings)
+        if obj.get("secondary") is not None:
+            obj["secondary"] = cls.fromDict(obj["secondary"])
+        obj["printings"] = map(Printing.fromDict, obj.get("printings", ()))
+        return cls(**obj)
 
     @property
     def color(self):
-        return sum([c.color for c in self.content], Color.COLORLESS)
+        if self.name == 'Ghostfire' or \
+                (self.text and 'devoid' in self.baseText.lower()):
+            return Color.COLORLESS
+        elif self.color_indicator is not None:
+            return self.color_indicator
+        else:
+            return Color.fromString(self.cost or '')
 
     @property
     def colorID(self):
-        return sum([c.colorID for c in self.content], Color.COLORLESS)
+        # Since Innistrad, cards that formerly said "[This card] is [color]"
+        # now have color indicators instead, so there's no need to check for
+        # such strings.
+        colors = self.color
+        txt = self.baseText or ''
+        # Reminder text is supposed to be ignored for the purposes of
+        # establishing color identity, though (as of Dark Ascension) Charmed
+        # Pendant and Trinisphere appear to be the only cards for which this
+        # makes a difference.
+        for c in Color.WUBRG:
+            if re.search(r'\{(./)?' + c.name + r'(/.)?\}', txt):
+                colors |= c
+        if self.isType('Land'):
+            # Basic land types aren't _de jure_ part of color identity, but
+            # rule 903.5d makes them a part _de facto_ anyway.
+            if self.isSubtype('Plains'):
+                colors |= Color.WHITE
+            if self.isSubtype('Island'):
+                colors |= Color.BLUE
+            if self.isSubtype('Swamp'):
+                colors |= Color.BLACK
+            if self.isSubtype('Mountain'):
+                colors |= Color.RED
+            if self.isSubtype('Forest'):
+                colors |= Color.GREEN
+        if self.secondary is not None:
+            colors |= self.secondary.colorID
+        return colors
 
     @property
     def cmc(self):
-        if self.cardClass == CardClass.flip:
-            return self.part1.cmc
-        else:
-            return sum(c.cmc for c in self.content)
+        if not self.cost:
+            return 0
+        cost = 0
+        for c in split_mana(self.cost)[0]:
+            m = re.search(r'(\d+)', c)
+            if m:
+                cost += int(m.group(1))
+            elif any(ch in c for ch in 'WUBRGSwubrgs'):
+                # This weeds out {X}, {Y}, etc.
+                cost += 1
+        return cost
 
     def devotion(self, to_color):
-        return tuple(c.devotion(to_color) for c in self.content)
+        if not self.cost:
+            return 0
+        devot = 0
+        for c in split_mana(self.cost)[0]:
+            c = Color.fromString(c)
+            if any(to_color & c):
+                devot += 1
+        return devot
 
+    ###
     @property
     def parts(self):
         return len(self.content)
 
+    ###
     @property
     def part1(self):
         return self.content[0]
 
+    ###
     @property
     def part2(self):
         return self.content[1] if len(self.content) > 1 else None
 
+    ###
     def isMultipart(self):
         return self.parts > 1
 
@@ -134,37 +185,33 @@ class Card:
     def inSet(self, set_):
         return [p for p in self.printings if p.set == set_]
 
-    name      = scalarField("name")
-    text      = scalarField("text")
-    power     = scalarField("power")
-    toughness = scalarField("toughness")
-    loyalty   = scalarField("loyalty")
-    hand      = scalarField("hand")
-    life      = scalarField("life")
-    color_indicator = scalarField("color_indicator")
-    type      = scalarField("type")
-    PT        = scalarField("PT")
-    HandLife  = scalarField("HandLife")
-    baseText  = scalarField("baseText")
+    @property
+    def type(self):
+        return ' '.join(self.supertypes + self.types +
+                        (('—',) + self.subtypes if self.subtypes else ()))
 
     @property
-    def supertypes(self):
-        return (t for c in self.content for t in c.supertypes)
-
-    @property
-    def types(self):
-        return (t for c in self.content for t in c.types)
-
-    @property
-    def subtypes(self):
-        return (t for c in self.content for t in c.subtypes)
-
-    @property
-    def cost(self):
-        if self.isNormal() or self.isSplit():
-            return sep.join(c.cost or '' for c in self.content)
+    def PT(self):
+        if self.power is not None:
+            return '%s/%s' % (self.power, self.toughness)
         else:
-            return self.part1.cost
+            return None
+
+    @property
+    def HandLife(self):
+        if self.hand is not None:
+            return '%s/%s' % (self.hand, self.life)
+        else:
+            return None
+
+    @property
+    def baseText(self):  # Returns rules text without reminder text
+        if self.text is None:
+            return None
+        txt = re.sub(r'\([^()]+\)', '', self.text)
+        # It is assumed that text is reminder text if & only if it's enclosed
+        # in parentheses.
+        return '\n'.join(filter(None, map(str.strip, txt.splitlines())))
 
     def isSupertype(self, type_):
         return type_ in self.supertypes
@@ -185,6 +232,7 @@ class Card:
 
     tagwidth = 8  # may be modified externally
 
+    ###
     def showField1(self, field, width=None):
         if not width:
             width = 79
@@ -246,6 +294,15 @@ class Card:
         if showSets:
             txt += self.showField1('sets', width)
         return txt
+
+    def __eq__(self, other):
+        return type(self) is type(other) and vars(self) == vars(other)
+
+    def __le__(self, other):
+        if type(self) is type(other):
+            return vars(self) <= vars(other)
+        else:
+            return NotImplemented
 
     def __repr__(self):
         return cheap_repr(self)
